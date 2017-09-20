d17e6565f7e731af4a1a979d0782c6bcedfea0d6
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-09-20 13:59:21 +0300

    Remove extra static file dirs

--- verboze/settings.py
+++ verboze/settings.py
@@ -134,8 +134,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
