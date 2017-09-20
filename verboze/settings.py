68d66bc3e3f0fa44f1d79754bdf59f6ffbe11171
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-09-20 14:18:48 +0300

    Add ALLOWED_HOSTS for local and production

--- verboze/settings.py
+++ verboze/settings.py
@@ -24,15 +24,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ON_HEROKU', False):
     DEBUG = False
+    ALLOWED_HOSTS = ['verboze.herokuapp.com']
 else:
     DEBUG = True
+    ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
