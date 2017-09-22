955d96c390dfe80ab0e25b2054eb78a16605365d
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-09-22 13:19:45 +0300

    Add www to verboze.com domain in allowed_hosts

--- verboze/settings.py
+++ verboze/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ON_HEROKU', False):
     DEBUG = False
-    ALLOWED_HOSTS = ['verboze.herokuapp.com', 'verboze.com']
+    ALLOWED_HOSTS = ['verboze.herokuapp.com', 'www.verboze.com']
 else:
     DEBUG = True
     ALLOWED_HOSTS = []
