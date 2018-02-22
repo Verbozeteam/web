b0c681a87258b77914410be3dd57bfd4265dde03
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-02-22 10:47:07 +0300

    Added sitemap

--- public_website/urls.py
+++ public_website/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from public_website.views import IndexPageView
+from public_website.views import IndexPageView, SitemapView
 
 
 urlpatterns = [
+	url(r'sitemap.xml', SitemapView.as_view(), name='sitemap'),
 	url(r'', IndexPageView.as_view(), name='index'),
 ]
--- public_website/views.py
+++ public_website/views.py
@@ -1,6 +1,11 @@
 from django.views.generic.base import TemplateView
-
+from django.http import HttpResponse
 
 class IndexPageView(TemplateView):
 
     template_name = "index.html"
+
+class SitemapView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(open('public_website/templates/sitemap.xml').read(), content_type='text/xml')
+
